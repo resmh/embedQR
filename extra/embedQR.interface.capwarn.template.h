2063919f58eeb31fd1875b2ea9d58f454617159d
Michael <resmh@users.noreply.github.com>
2021-07-23 13:51:23 +0200

    Add Interface Template for Captive Portal Interference Warning

---
+++ extra/embedQR.interface.capwarn.template.h
@@ -0,0 +1,3 @@
+const char HTTP_SCRIPT_EMBEDQR_CAPWARN[] PROGMEM =
+"<a href='http://%s' target='_blank' id='qra' style='display: none; color: white; font-style: italic; text-align: justify;'>%s %s</a><br />"
+"";
